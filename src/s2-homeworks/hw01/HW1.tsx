84d47b03f62eda784192f1e2c5f21e9015a52339
Alexander Korobov <dktwho@mail.ru>
2023-06-25 15:56:03 +0300

    add logic for component Message.tsx, FriendMessage.tsx

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Message from './message/Message'
 import MessageSender from './message-sender/MessageSender'
+import Message from "./message/Message";
 import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
+
 
 /*
 * 1 - описать тип MessageType
@@ -14,17 +15,32 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number;
+    user: UserType;
+    message: MessageContentType;
+}
+
+export type UserType = {
+    avatar: string;
+    name: string
+}
+
+export type MessageContentType = {
+    text: string;
+    time: string
+}
+
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Bill',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'some text some  text', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,7 +48,7 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Bob', // можно менять
     },
     message: {
         text: 'зеркальное сообщение для тренировки css', // можно менять
@@ -40,19 +56,21 @@ export const friendMessage0: MessageType = {
     },
 }
 
+
 const HW1 = () => {
+    // @ts-ignore
     return (
         <div id={'hw1'}>
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
-                    <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
+                    <Message message={message0}/>
+                    <FriendMessage message={friendMessage0}/>
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
+                <MessageSender M={Message}/>
             </div>
         </div>
     )
