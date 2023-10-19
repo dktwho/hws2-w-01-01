3f7985c3252488115b30b05d7e9ce819b716d671
Alexander Korobov <dktwho@mail.ru>
2023-10-19 16:46:54 +0300

    3 attempts - hw13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -37,29 +37,28 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок) ')
-                setInfo('код 200 - обычно означает что скорее всего всё ок)')
+                setText('...всё ок) код 200 - обычно означает что скорее всего всё ок)')
+                setInfo('')
                 // дописать
             })
             .catch((e) => {
                 if (x === false) {
-                    setCode('500')
+                    setCode('Ошибка 400!')
                     setImage(error400)
                     setText('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
-                    setInfo('Ошибка 400!')
-
+                    setInfo('')
                 }
                 if (x === undefined) {
                     setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных')
-                    setCode('400')
+                    setCode('Ошибка 500!')
                     setImage(error500)
-                    setInfo('Ошибка 500!')
+                    setInfo('')
                 }
                 if (x === null) {
                     setCode('Error!')
                     setImage(errorUnknown)
                     setText('Network Error AxiosError')
-                    setInfo('Error')
+                    setInfo('')
                 }
             })
     }
